aaf147491bda47c25182492c6e7ca1eb82751745
eduardobcoelho <eduardobcoelho27@outlook.com>
2020-05-15 11:48:30 -0300

    transformNumber function

--- js/main.js
+++ js/main.js
@@ -1,21 +1,41 @@
-var display = document.querySelector(".display"); // display
+var i = 0; // index
+ 
+var display = document.querySelector(".display"); // display div HTML element
 
-var arrayNumber = []; // array used for concatenate numbers
+var numbersClicked = []; // array used to store the number clicked; it will be used to concatenate numbers latelly
 
-function showNumber(numValue) {
-    let storeValue = Number(numValue.value); // storing the value of the .btn-number in a variable
-    arrayNumber.push(storeValue); // adding the value in the next position of array
-    display.insertAdjacentHTML("beforeend", arrayNumber[(arrayNumber.length)-1]); // inserting at the display element the stored value
-}
+var arrayNumbers = []; // array used to store the concatenation result of the function transformNumber
+ 
+function showNumber(numValue){ // function used to show in display the last number button value clicked
 
-function showOp(opValue) {
+    let storeValue = numValue.value; // stores in a variable the value of the last button clicked
 
-    arrayNumber = [] // reseting the array
+    numbersClicked.push(storeValue); // stores in the array next position the value of the last button clicked
 
-    let storeValue = String(opValue.value); // storing the string that represents the operation
-
-    display.append(" ")
-    display.insertAdjacentHTML("beforeend", storeValue); // inserting the op string at the display
-    display.append(" ")
+    display.insertAdjacentHTML("beforeend", numbersClicked[(numbersClicked.length)-1]); // insert at the end of the display div the last button clicked
 
 }
+
+function showOp(opValue){ // function used to show in display the last operation button value clicked
+
+    let storeValue = String(opValue.value); // stores in a variable the value of the last button clicked
+
+    display.append(" "); // add a space before the storeValue
+    display.insertAdjacentHTML("beforeend", storeValue); // insert at the end of the display div the last operation button value clicked
+    display.append(" "); // add a space after the storeValue
+
+}
+
+function transformNumber(){ // function used to take the array numbersClicked numbers and transform in one number and store into arrayNumbers 
+
+    let transformedNumber = ""; // variable used to concatenate the numbers
+
+    for (i = 0; i<numbersClicked.length; i++){ // "for" used to take all the elements of numbersClicked array 
+        transformedNumber = transformedNumber + numbersClicked[i]; // concatenating the elements of numbersClicked array
+    }
+
+    arrayNumbers.push(Number.parseInt(transformedNumber)); // pushing into the arrayNumbers the concatenated element(transformedNumber) in number form
+    numbersClicked = [] // reseting numbersClicked array
+}
+
+// test area
