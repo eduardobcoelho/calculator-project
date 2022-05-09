3d13575965335f5437313ec5eb09ff71a882bbe9
eduardobcoelho <luizbcoelho27@outlook.com>
2022-05-09 17:18:37 -0300

    feat: added function to set button eventListener

--- js/main.js
+++ js/main.js
@@ -1,95 +1,12 @@
-const display = document.getElementById('display');
-const btnNumbersConjuntsValue = {
-  0: [1, 2, 3],
-  1: [4, 5, 6],
-  2: [7, 8, 9],
-  3: [0, '.'],
-};
-const btnOpsConjuntsValue = {
-  0: ['sum', 'subtraction', 'multiplication', 'division'],
-  1: ['CE', 'C', '='],
-};
+const buttons = document.getElementsByTagName('button');
 
-renderBtnNumbersConjunts();
-renderBtnOpsConjunts();
+addButtonsClickListener();
 
-function renderBtnNumbersConjunts() {
-  const containerNumbers = document.getElementById('container-numbers');
-  const conjunts = getConjunts('btn-numbers-conjunt', 4);
-  conjunts.forEach((conjunt) => {
-    containerNumbers.appendChild(conjunt);
-  });
-}
-
-function renderBtnOpsConjunts() {
-  const containerOps = document.getElementById('container-ops');
-  const conjunts = getConjunts('btn-op-conjunt', 2);
-  conjunts.forEach((conjunt) => {
-    containerOps.appendChild(conjunt);
-  });
-}
-
-function getConjunts(classType, quantity) {
-  const conjunts = createConjunts(classType, quantity);
-  conjunts.forEach((conjunt, i) => {
-    const conjuntValue =
-      classType === 'btn-numbers-conjunt'
-        ? btnNumbersConjuntsValue[i]
-        : btnOpsConjuntsValue[i];
-    conjuntValue.map((value) => {
-      const button = createBtn(
-        classType === 'btn-numbers-conjunt' ? 'btn-number' : 'btn-op',
-        value,
-      );
-      conjunt.appendChild(button);
+function addButtonsClickListener() {
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
+    button.addEventListener('click', () => {
+      console.log('clicou no botão: ', button.value);
     });
-  });
-  return conjunts;
-}
-
-function createConjunts(classType, quantity) {
-  let conjunts = [];
-  for (let i = 0; i < quantity; i++) {
-    const div = document.createElement('div');
-    div.classList.add(classType);
-    conjunts.push(div);
   }
-  return conjunts;
-}
-
-function createBtn(classType, value) {
-  const button = document.createElement('button');
-  button.classList.add(classType, 'btn-style');
-  isBtnAction(value) ? button.classList.add('btn-action') : '';
-  const btnValue = getValueByExceptions(value);
-  button.value = btnValue;
-  button.textContent = btnValue;
-  button.style = getStyleByExceptions(value);
-  return button;
-}
-
-function isBtnAction(value) {
-  const actions = ['CE', 'C', '='];
-  return actions.includes(value);
-}
-
-function getValueByExceptions(value) {
-  const exceptions = {
-    sum: '+',
-    subtraction: '-',
-    multiplication: 'x',
-    division: '/',
-  };
-  return exceptions[value] || value;
-}
-
-function getStyleByExceptions(value) {
-  const exceptions = {
-    '0': 'width: 65%',
-    '.': 'width: 30%',
-    'CE': 'background-color: #5007ed; flex-basis: 20%',
-    'C': 'background-color: #5007ed; flex-basis: 20%',
-    '=': 'background-color: #e63819; flex-basis: 45%',
-  };
-  return exceptions[value] || '';
 }
---
+++ js/onMounted.js
@@ -0,0 +1,95 @@
+const display = document.getElementById('display');
+const btnNumbersConjuntsValue = {
+  0: [1, 2, 3],
+  1: [4, 5, 6],
+  2: [7, 8, 9],
+  3: [0, '.'],
+};
+const btnOpsConjuntsValue = {
+  0: ['sum', 'subtraction', 'multiplication', 'division'],
+  1: ['CE', 'C', '='],
+};
+
+renderBtnNumbersConjunts();
+renderBtnOpsConjunts();
+
+function renderBtnNumbersConjunts() {
+  const containerNumbers = document.getElementById('container-numbers');
+  const conjunts = getConjunts('btn-numbers-conjunt', 4);
+  conjunts.forEach((conjunt) => {
+    containerNumbers.appendChild(conjunt);
+  });
+}
+
+function renderBtnOpsConjunts() {
+  const containerOps = document.getElementById('container-ops');
+  const conjunts = getConjunts('btn-op-conjunt', 2);
+  conjunts.forEach((conjunt) => {
+    containerOps.appendChild(conjunt);
+  });
+}
+
+function getConjunts(classType, quantity) {
+  const conjunts = createConjunts(classType, quantity);
+  conjunts.forEach((conjunt, i) => {
+    const conjuntValue =
+      classType === 'btn-numbers-conjunt'
+        ? btnNumbersConjuntsValue[i]
+        : btnOpsConjuntsValue[i];
+    conjuntValue.map((value) => {
+      const button = createBtn(
+        classType === 'btn-numbers-conjunt' ? 'btn-number' : 'btn-op',
+        value,
+      );
+      conjunt.appendChild(button);
+    });
+  });
+  return conjunts;
+}
+
+function createConjunts(classType, quantity) {
+  let conjunts = [];
+  for (let i = 0; i < quantity; i++) {
+    const div = document.createElement('div');
+    div.classList.add(classType);
+    conjunts.push(div);
+  }
+  return conjunts;
+}
+
+function createBtn(classType, value) {
+  const button = document.createElement('button');
+  button.classList.add(classType, 'btn-style');
+  isBtnAction(value) ? button.classList.add('btn-action') : '';
+  const btnValue = getValueByExceptions(value);
+  button.value = btnValue;
+  button.textContent = btnValue;
+  button.style = getStyleByExceptions(value);
+  return button;
+}
+
+function isBtnAction(value) {
+  const actions = ['CE', 'C', '='];
+  return actions.includes(value);
+}
+
+function getValueByExceptions(value) {
+  const exceptions = {
+    sum: '+',
+    subtraction: '-',
+    multiplication: 'x',
+    division: '/',
+  };
+  return exceptions[value] || value;
+}
+
+function getStyleByExceptions(value) {
+  const exceptions = {
+    '0': 'width: 65%',
+    '.': 'width: 30%',
+    'CE': 'background-color: #5007ed; flex-basis: 20%',
+    'C': 'background-color: #5007ed; flex-basis: 20%',
+    '=': 'background-color: #e63819; flex-basis: 45%',
+  };
+  return exceptions[value] || '';
+}
